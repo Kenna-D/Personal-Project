232496336e75db120f427f1ef6ad837782127144
Makenna Garrett <kenna.dunford@gmail.com>
2021-04-06 10:09:22 -0600

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from './Components/Home';
+import AllProducts from './Components/AllProducts';
+import Cart from './Components/Cart';
+import OrderHistory from './Components/OrderHistory';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import Product from './Components/Product';
+
+
+export default (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route path="/all-products" component={AllProducts} />
+    <Route path="/cart" component={Cart} />
+    <Route path="/order-history" component={OrderHistory} />
+    <Route path="/login" component={Login} />
+    <Route path="/register" component={Register} />
+    <Route path="/product" component={Product} />
+  </Switch>
+)
